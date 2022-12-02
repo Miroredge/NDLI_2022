990f9c1535729040b63158f48f6a0961b9647a12
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-12-02 01:35:26 +0100

    working keypress system

--- Site/keypress.js
+++ Site/keypress.js
@@ -1,7 +1,22 @@
 
 
 function OnKeyPress(e) {
+    let key = e.key;
     console.log(e.key);
+
+    switch (key) {
+        case '1':
+            window.location = "index.html";
+            break;
+        case '2':
+            window.location = "Infos.html";
+            break;
+        case '3':
+            window.location = "Credit.html"
+        default:
+            // TODO: remove
+            break;
+    }
 }
 
 
