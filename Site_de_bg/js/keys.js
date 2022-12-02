547578a10676b927f5d21854f0747541e4b1b7e9
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-12-02 03:26:44 +0100

    end konami code

--- Site_de_bg/js/keys.js
+++ Site_de_bg/js/keys.js
@@ -1,7 +1,39 @@
+var list_keys = [];
 
-function OnKeyPress(e) {
+
+function arrayEqual(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
+function is_konami(l) {
+    // const up = ["ArrowUp", "ArrowUp"];
+    // const down  = ["ArrowDown", "ArrowDown"];
+    // const left  = ["ArrowLeft", "ArrowLeft"];
+    // const right = ["ArrowRight", "ArrowRight"];
+    // const a = ["a", "a"];
+    // const b = ["b", "b"];
+    // const konami = [].concat(up, up, down, down, left, right, left, right, b, a);
+    const konami = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+    console.log("konami")
+    console.log(konami);
+    return arrayEqual(l, konami);
+}
+
+window.addEventListener('keydown',
+function(e) {
     let key = e.key;
-    console.log(e.key);
+    list_keys.push(key);
+    // keep only 20 events (that is 10 keypresses) at most
+    list_keys = list_keys.slice(-10);
+    if ( is_konami(list_keys) ) {
+        window.location = "konami.html";
+        return;
+    }
+
+    console.log(list_keys);
 
     switch (key) {
         case '1':
@@ -14,10 +46,10 @@ function OnKeyPress(e) {
             window.location = "index.html#contact";
             break;
         default:
-            // TODO: remove
+            console.log(key);
             break;
     }
-}
+}, false);
 
 
 
